800e419c656f58c5ce154115e16eba83cd91d08b
Niyaz Khasanov <khasanchik85@gmail.com>
2024-10-10 01:20:57 +0500

    Был создан файл ccdb.py

---
+++ users/management/commands/ccdb.py
@@ -0,0 +1,22 @@
+from django.core.management import BaseCommand
+import pyodbc
+from config.settings import DATABASE, USER, PASSWORD, HOST
+
+class Command(BaseCommand):
+
+    def handle(self, *args, **options):
+        ConnectionString = f'''DRIVER={{ODBC Driver 17 for SQL Server}};
+                                        SERVER={HOST};
+                                        DATABASE={DATABASE};
+                                        UID={USER};
+                                        PWD={PASSWORD}'''
+        try:
+            conn = pyodbc.connect(ConnectionString)
+            conn.autocommit = True
+            conn.execute(fr"CREATE DATABASE Shelter320;")
+        except pyodbc.ProgrammingError as ex:
+            print(ex)
+        else:
+            print("База данных Shelter320;")
+        finally:
+            conn.close()
